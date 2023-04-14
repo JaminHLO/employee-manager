70eaf1b011a6baa00124e3d8dc1d799eddc0715d
Jaimon <jamin.hogan@gmail.com>
2023-04-14 13:38:21 -0400

    added comments, removed debugging console.log, added README content

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# employee-manager
+# Employee-Manager
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+## Description
+    
+The Project is a employee manager that organiza a company's departments, roles, employees and managers. It is built with javascript with a SQL backend. It uses the inquirer, asciiart-logo, dotenv, and mysql libraries and operates from the CLI. 
+    
+## Installation
+
+Users will need to install the npm libraries as defined in the included package.json file. Also please create a .env file including entries for the employee_db database as well as your mysql username and password.
+Set-Up CLI Commands from project root directory:
+>npm i 
+>cd db
+>mysql -u root -p // when prompted enter your mysql password
+>SOURCE schema.sql
+>SOURCE seeds.sql
+>cd ..
+>node index.js
+
+## Usage
+
+A Demonstration Video showing the setup and functionality of the program is available here: <https://www.loom.com/share/5ae6c3890741488e98adc126cd24df96> 
+Otherwise, the program is initialized from the CLI in the project root directory with:
+>node index.js
+
+## License
+
+This Project is covered by the following license: GNU General Public License v3.0.
+
+## Contributions
+
+Instruction was provided by Instructor Saurav with assistance from TAs Andreas #1, Andreas #2, Constan, and Morgan. 
+
+Leaned on previous assignments and online resources. A good online resource I used was: 
+<https://www.w3schools.com/>
+
+For a good resource for javascript debugging and error messages, check out: 
+<https://stackoverflow.com/>
+
+## Tests
+
+The Project may tested running 'node index.js'and then using MySQL WOrkbench 6.0 CE to confirm the status of db. Please see the Demonstration Video above for a walkthrough.
+
+## Questions
+
+My GitHub username is JaminHLO and my repository is available here: <https://github.com/JaminHLO/>.
+If you have any questions please contact me at <jamin.hogan@gmail.com>.
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ViewManager = require('./lib/viewManager');
 const AddManager = require('./lib/addManager');
 require('console.table');
 
+// const to allow easy addition of choices to main menu
 const mainMenuObject = {
     type: 'list',
     name: 'choice',
@@ -15,14 +16,114 @@ const mainMenuObject = {
         'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
 };
 
+// function that creates SQL query and updates entries in database
+// @queryType: what we're updating
+// @questions: what we're asking user
+// @emmployeeArray: array of employees to search for an id
+// @toSearchArray: array of either roles or managers to search for an id
+function promptUserUpdate (queryType, questions, emmployeeArray, toSearchArray) {
+    // start prompt of update questions
+    inquirer.prompt(questions).then(async (answers) => {
+        let queryString = `UPDATE employee `;
+        // console.log(`We're going to ADD to ${queryType}`);
+        // console.log(answers);
+        let employee_id = 1 + (emmployeeArray.indexOf(answers.employee));
+        switch(queryType){
+            case "role":
+                let role_id = 1 + (toSearchArray.indexOf(answers.role));
+                queryString += `SET role_id = ${role_id}
+                WHERE id = ${employee_id};`;
+                break;
+            case "manager":
+                let manager_id = 1 + (toSearchArray.indexOf(answers.manager));
+                queryString += `SET manager_id = ${manager_id}
+                WHERE id = ${employee_id};`;
+                break;
+            default: 
+                console.error("error in promptUserUpdate switch");
+        }
+        // console.log(`sql query:\n${queryString}`);
+        db.query(queryString, function (err, results) {
+            if(err) throw err;
+            // console.log(results);
+            console.log(`Updated employee's role.`);
+            mainMenu();
+        });
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("\nYour console environment is not supported!");
+        } else {
+            console.log(error);
+        }
+    });
+}
+// function that creates SQL query and adds entries to database
+// @queryType: what we're updating
+// @questions: what we're asking user
+// @toSearchArray: array of departments or roles to search for an id
+// @managerArray: optional array that may contain managers to search for an id
+function promptUserAdd (queryType, questions, toSearchArray, managerArray) {
+    // start prompt of add questions
+    inquirer.prompt(questions).then(async (answers) => {
+        let successString = "";
+        let queryString = `INSERT INTO ${queryType} `;
+        // console.log(`We're going to ADD to ${queryType}`);
+        // console.log(answers);
+        switch(queryType){
+            case "department":
+                queryString += `(name) 
+                VALUES ("${answers.name}");`;
+                successString = answers.name;
+                break;
+            case "role":
+                let department_id = 1 + (toSearchArray.indexOf(answers.department));
+                //department_id needs to be sent instead of department name
+                queryString += `(title, salary, department_id) 
+                VALUES ("${answers.title}", ${answers.salary}, ${department_id});`;
+                successString = answers.title;
+                break;
+            case "employee":
+                let role_id = 1 + (toSearchArray.indexOf(answers.role));
+                let manager_id = 1 + (managerArray.indexOf(answers.manager));
+                //role_id and manager_id need to be sent instead of string equivalents
+                queryString += `(first_name, last_name, role_id, manager_id) 
+                VALUES ("${answers.first_name}", "${answers.last_name}", ${role_id}, ${manager_id});`;
+                successString = `${answers.first_name} ${answers.last_name}`;
+                break;
+            default: 
+                console.error("error in promptUserAdd switch");
+        }
+        // console.log(`sql query:\n${queryString}`);
+        db.query(queryString, function (err, results) {
+            if(err) throw err;
+            // console.log(results);
+            console.log(`Added ${successString} to the database.`);
+            mainMenu();
+        });
+
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("\nYour console environment is not supported!");
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+// function that users helper functions and classes to create SQL queries and view and modify db
+// @answer: user selected action from main menu
 function queryDB (answer) {
     let {choice} = answer;
     choice = choice.toLowerCase(); 
-    let queryString = ""
+    let queryString = "";
+    // user wants to view  
     if (choice.includes("view")){
         const newViewQuery = new ViewManager(choice);
         queryString = newViewQuery.toQuery();
         // console.log(`queryString is ${queryString}`);
+    // user wants to add to db 
     } else if (choice.includes("add")) {
         // console.log("\nwe're going to Add with XX");
         const newAddQuery = new AddManager(choice);
@@ -33,8 +134,7 @@ function queryDB (answer) {
             let addQueryList = {};
             db.query(addQuery, function (err, results) {
                 if(err) throw err;
-
-                //test logic for choice
+                // user wants to add a role 
                 if (choice.includes("role")){
                     results.forEach(department => optionsArray.push(department.name));
                     // console.log(optionsArray);
@@ -46,6 +146,7 @@ function queryDB (answer) {
                     addQuestions.unshift(addQueryList);
                     // console.log(addQuestions);
                     promptUserAdd("role", addQuestions, optionsArray); 
+                // user wants to add an employee
                 } else if (choice.includes("employee")){
                     results.forEach(role => optionsArray.push(role.title));
                     // add employee role list
@@ -78,13 +179,13 @@ function queryDB (answer) {
                 }
      
             });
+         // user wants to add a department
         } else {
             promptUserAdd("department", addQuestions); 
         }
-        
-
+    // user wants to update database
     } else if (choice.includes("update")) {
-        console.log("\nwe're going to Update with XX");
+        // console.log("\nwe're going to Update with XX");
            // select employee to update
         let updateQuestions = [];
         db.query('SELECT first_name, last_name FROM employee', function (err, employees) {
@@ -98,7 +199,8 @@ function queryDB (answer) {
                 message: 'Which employee\'s role do you want to update?',
                 choices: employeeArray};
             updateQuestions.push(updateQueryList);
-            console.log(updateQuestions);
+            // console.log(updateQuestions);
+            // user wants to update an employee's role 
             if (choice.includes("role")) {
                 db.query('SELECT title FROM role', function (err, roles) {
                     let roleArray = [];
@@ -112,6 +214,7 @@ function queryDB (answer) {
                     updateQuestions.push(updateQueryList2);
                     promptUserUpdate("role", updateQuestions, employeeArray, roleArray);
                 });
+            // user wants to update an employee's manager 
             } else if (choice.includes("manager")) {
                 let updateQueryList2 = {
                     type: 'list',
@@ -140,105 +243,9 @@ function queryDB (answer) {
             mainMenu();
         });
     }
-    // mainMenu();
 }
 
-function promptUserUpdate (queryType, questions, array1, array2) {
-    // start prompt of update questions
-    inquirer.prompt(questions).then(async (answers) => {
-        // let successString = "";
-        let queryString = `UPDATE employee `;
-        // console.log(`We're going to ADD to ${queryType}`);
-        console.log(answers);
-        let employee_id = 1 + (array1.indexOf(answers.employee));
-        switch(queryType){
-            // case "department":
-            //     queryString += `(name) 
-            //     VALUES ("${answers.name}");`;
-            //     successString = answers.name;
-            //     break;
-            case "role":
-                let role_id = 1 + (array2.indexOf(answers.role));
-                queryString += `SET role_id = ${role_id}
-                WHERE id = ${employee_id};`;
-                // successString = answers.title;
-                break;
-            case "manager":
-                let manager_id = 1 + (array2.indexOf(answers.manager));
-                queryString += `SET manager_id = ${manager_id}
-                WHERE id = ${employee_id};`;
-                // successString = `${answers.first_name} ${answers.last_name}`;
-                break;
-            default: 
-                console.error("error in promptUserUpdate switch");
-        }
-        console.log(`sql query:\n${queryString}`);
-        db.query(queryString, function (err, results) {
-            if(err) throw err;
-            console.log(results);
-            console.log(`Updated employee's role.`);
-            mainMenu();
-        });
-
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.log("\nYour console environment is not supported!");
-        } else {
-            console.log(error);
-        }
-    });
-}
-
-function promptUserAdd (queryType, questions, array1, array2) {
-    // start prompt of add questions
-    inquirer.prompt(questions).then(async (answers) => {
-        let successString = "";
-        let queryString = `INSERT INTO ${queryType} `;
-        console.log(`We're going to ADD to ${queryType}`);
-        console.log(answers);
-        switch(queryType){
-            case "department":
-                queryString += `(name) 
-                VALUES ("${answers.name}");`;
-                successString = answers.name;
-                break;
-            case "role":
-                let department_id = 1 + (array1.indexOf(answers.department));
-                //department_id needs to be sent instead of department name
-                queryString += `(title, salary, department_id) 
-                VALUES ("${answers.title}", ${answers.salary}, ${department_id});`;
-                successString = answers.title;
-                break;
-            case "employee":
-                let role_id = 1 + (array1.indexOf(answers.role));
-                let manager_id = 1 + (array2.indexOf(answers.manager));
-                //role_id and manager_id need to be sent instead of string equivalents
-                queryString += `(first_name, last_name, role_id, manager_id) 
-                VALUES ("${answers.first_name}", "${answers.last_name}", ${role_id}, ${manager_id});`;
-                successString = `${answers.first_name} ${answers.last_name}`;
-                break;
-            default: 
-                console.error("error in promptUserAdd switch");
-        }
-        // console.log(`sql query:\n${queryString}`);
-        db.query(queryString, function (err, results) {
-            if(err) throw err;
-            console.log(results);
-            console.log(`Added ${successString} to the database.`);
-            mainMenu();
-        });
-
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.log("\nYour console environment is not supported!");
-        } else {
-            console.log(error);
-        }
-    });
-}
-
+// display mainMenu based on const object mainMenuObject
 function mainMenu () {
     // start prompt of questions
     inquirer.prompt(mainMenuObject).then(async (answers) => {
@@ -253,6 +260,7 @@ function mainMenu () {
     })
 }
 
+// basic init function to display out ascii logo then mainMenu
 function init() {
     // display ascii logo
     console.log(logo(config).render());
